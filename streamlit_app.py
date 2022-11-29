8f39168cc38b8e6b3204117aacd85981d3849080
marianaP99 <113610725+marianaP99@users.noreply.github.com>
2022-11-29 19:32:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ sound_df = pd.read_csv("sonograme.csv", names = ['Tempo','Onda'])
 st.line_chart(sound_df['Onda'], width = max(sound_df['Tempo']))
 
 sound_df = pd.read_csv("features.csv")
-st.bar_chart(chart_data)
+st.bar_chart(sound_df)
 
 
 
