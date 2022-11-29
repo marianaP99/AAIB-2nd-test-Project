eeb7ca0c88aa0dd52d84e1dc43313fbfc06b5bf6
marianaP99 <113610725+marianaP99@users.noreply.github.com>
2022-11-29 19:23:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,7 @@
-import streamlit as st
-import pandas as pd
-
 """ 
 # hang in there :') 
 """
-import pandas as pd
-import numpy as np
 
-sound_df = pd.read_csv("sonograme.csv")
+import streamlit as st
 
-st.line_chart(sound_df['Onda'], width = max(sound_df['Tempo']))
-
-# sound_df = pd.read_csv("sonograme.csv", names = ['Tempo','Onda'])
-# st.bar_chart(chart_data)
+streamlit run main.py
