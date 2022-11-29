b98eb2f920851f5aa503ddd1ece185f8953c1bfd
marianaP99 <113610725+marianaP99@users.noreply.github.com>
2022-11-29 19:08:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ import pandas as pd
 import pandas as pd
 import numpy as np
 
-test = pd.read_csv("sonograme2.csv", names = ['Tempo','Onda'])
+test = pd.read_csv("sonograme.csv", names = ['Tempo','Onda'])
 
 st.line_chart(x=test['Tempo'],y=test['Onda'])
