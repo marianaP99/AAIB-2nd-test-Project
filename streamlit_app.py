963988f016e801428f583fe263c5cfe739926986
marianaP99 <113610725+marianaP99@users.noreply.github.com>
2022-11-29 19:02:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib as plt
 
 """ 
 # Hello World!! 
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
