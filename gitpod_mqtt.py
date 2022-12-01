26878f77a5f5d671f804961735f017c844a078af
marianaP99 <ms.pinto@campus.fct.unl.pt>
2022-12-01 18:22:50 +0000

    up

--- gitpod_mqtt.py
+++ gitpod_mqtt.py
@@ -1,24 +1,30 @@
 import paho.mqtt.client as mqtt
 import recordsound as rs
+import streamlit_app as myapp
 
 def on_connect(client, userdata, flags, rc):
-     logging.info("Connected flags"+str(flags)+"result code "\
-     +str(rc)+"client1_id ")
-     client.connected_flag=True
+     print("Connected flags"+str(flags)+"result code " +str(rc)+"client1_id ")
+
+def on_message(client, userdata, msg):
+     print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("test.mosquitto.org", 1883, 600)
 
-def mqtt_sub():      
-    client.loop_forever()
+client.connect("test.mosquitto.org", 1883)
+button = myapp.button
+
+def mqtt_sub():
+    message = client.subscribe("AAIB/MP")
 
 def mqtt_pub():
-    message = rs.record
-    client.publish("AAIB/test", message)
+    message = 'start'
+    client.publish("AAIB/MP", message)
 
-button = mqtt_sub()
+loop_start()
 
 if button:
     mqtt_pub()
+    mqtt_pub()
+    rs.save_file(message)
--- pc_mqtt.py
+++ pc_mqtt.py
@@ -2,25 +2,26 @@ import paho.mqtt.client as mqtt
 import recordsound as rs
 
 def on_connect(client, userdata, flags, rc):
-     logging.info("Connected flags"+str(flags)+"result code "\
-     +str(rc)+"client1_id ")
-     client.connected_flag=True
+     print("Connected flags"+str(flags)+"result code " +str(rc)+"client1_id ")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+     print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("test.mosquitto.org", 1883, 600)
 
-client.loop_forever()
+client.connect("test.mosquitto.org", 1883)
+
+def mqtt_sub():
+    print("Ready to start aquisition")
+    button = client.subscribe("AAIB/MP")
 
 def mqtt_pub():
     message = rs.record
-    client.publish("AAIB/test", message)
+    client.publish("AAIB/MP", message)
 
-button = mqtt_sub()
+loop_start()
 
-if button:
+if button == 'START':
     mqtt_pub()
