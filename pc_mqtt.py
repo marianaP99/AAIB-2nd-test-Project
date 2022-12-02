0ca442182fc8a6753c0c153de59c948097c9bf01
marianaP99 <ms.pinto@campus.fct.unl.pt>
2022-12-02 13:50:13 +0000

    up

--- pc_mqtt.py
+++ pc_mqtt.py
@@ -2,16 +2,10 @@ import paho.mqtt.client as mqtt
 import recordsound as rs
 
 def on_connect(client, userdata, flags, rc):
-     print("Connected flags"+str(flags)+"result code " +str(rc)+"client1_id ")
+     print("Connected")
 
 def on_message(client, userdata, msg):
      print(msg.topic+" "+str(msg.payload))
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect("test.mosquitto.org", 1883)
 
 def mqtt_sub():
     print("Ready to start aquisition")
@@ -21,7 +15,13 @@ def mqtt_pub():
     message = rs.record
     client.publish("AAIB/MP/SOUND", message)
 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("test.mosquitto.org", 1883)
+
 client.loop_start()
 
 if button == 'start':
-    mqtt_pub()
+    mqtt_pub
