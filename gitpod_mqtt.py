31583321d26e8c8e43c9b479bf7e5fc74e083bd8
marianaP99 <ms.pinto@campus.fct.unl.pt>
2022-12-02 15:36:44 +0000

    x

--- gitpod_mqtt.py
+++ gitpod_mqtt.py
@@ -17,6 +17,4 @@ client.on_publish = on_publish
 client.connect("broker.hivemq.com", 1883)
 
 client.loop_forever()
-
 client.publish("AAIB/MP/READY", payload = 'start')
-client.subscribe("AAIB/MP/SOUND")
